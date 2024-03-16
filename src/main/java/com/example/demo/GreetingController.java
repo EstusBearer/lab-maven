7e1b6af94047b83a6ff112340f7d8f4866b9859c
ruslan <glad2os.dev@gmail.com>
2024-03-16 18:56:58 -0400

    Added Dockerfile

--- src/main/java/com/example/demo/GreetingController.java
+++ src/main/java/com/example/demo/GreetingController.java
@@ -1,2 +1,14 @@
-package com.example.demo;public class GreetingController {
+package com.example.demo;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class GreetingController {
+    @RequestMapping("/")
+    public ResponseEntity<String> Test() {
+        return new ResponseEntity<>("Hello World!", HttpStatus.OK);
+    }
 }
